1029727cc5a3201a6e2e229e82c394aec1287261
Hugo Le Henaff <hle-hena@c2r1p1.42perpignan.fr>
2024-11-06 15:02:32 +0100

    strlen_tester done

--- libft_tester/ft_strlen_test.c
+++ libft_tester/ft_strlen_test.c
@@ -6,20 +6,42 @@
 /*   By: hle-hena <hle-hena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:18:52 by hle-hena          #+#    #+#             */
-/*   Updated: 2024/11/06 14:33:48 by hle-hena         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:55:50 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <stdio.h>
 
-int	ft_strlen_test(char *arg)
+void	ft_strlen_test(int index, char *arg, size_t expected)
 {
-	
+	size_t	res;
+
+	res = ft_strlen(arg);
+	if (res == expected)
+		printf("\033[32mTest %d : OK\n\033[0m", index);
+	else
+		printf("\033[31mTest %d : KO\n\033[0m", index);
 }
 
 int main()
 {
-	printf("Size is %ld\n", ft_strlen("test"));
+	int	i = -1;
+	char	*tests[] = 
+	{
+		"This is a normal test",
+		"This a lonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnng test",
+		"",
+		NULL
+	};
+	int	expects[] =
+	{
+		21,
+		104,
+		0,
+		0
+	};
+	while (tests[++i])
+		ft_strlen_test(i, tests[i], expects[i]);
 	return (0);
 }
