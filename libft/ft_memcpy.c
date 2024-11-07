4edd02b1ab6a98f4ef5ea213bfa741a283dda933
Hugo Le Henaff <hle-hena@c2r3p4.42perpignan.fr>
2024-11-07 17:43:30 +0100

    Fixed some crash

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hle-hena <hle-hena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 09:05:46 by hle-hena          #+#    #+#             */
-/*   Updated: 2024/11/05 09:49:44 by hle-hena         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:28:15 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!dest && !src)
+		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
