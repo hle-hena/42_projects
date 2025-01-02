c4247de34ea49084e543e6e217cf34f1a7b088ef
hle-hena <hugoleh@icloud.com>
2025-01-02 13:00:12 +0100

    Added fov

--- fdf/srcs/projection.c
+++ fdf/srcs/projection.c
@@ -6,7 +6,7 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 13:58:00 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/01/02 11:57:50 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/01/02 12:44:41 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,21 @@ int	get_iso(t_data *data, t_line *line, t_vec *start, t_vec *end)
 
 int	get_persp(t_data *data, t_line *line, t_vec *start, t_vec *end)
 {
+	float fov_scale_x;
+	float fov_scale_y;
+
+    fov_scale_x = data->win_wid / (2.0f * tan(30 / 2.0f));
+    fov_scale_y = data->win_len / (2.0f * tan(30 / 2.0f));
 	if (start->z < 1 && end->z < 1)
 		return (0);
 	if (start->z < 1)
 		change_point(start, *end);
 	else if (end->z < 1)
 		change_point(end, *start);
-	line->start = (t_point){(start->x) / start->z + (data->win_wid / 2),
-		(start->y) / start->z + (data->win_len / 2), 0, (t_col){0, 0, 0}};
-	line->end = (t_point){(end->x) / end->z + (data->win_wid / 2),
-		(end->y) / end->z + (data->win_len / 2), 0, (t_col){0, 0, 0}};
+	line->start = (t_point){(start->x * fov_scale_x) / start->z + (data->win_wid / 2),
+		(start->y * fov_scale_y) / start->z + (data->win_len / 2), 0, (t_col){0, 0, 0}};
+	line->end = (t_point){(end->x * fov_scale_x) / end->z + (data->win_wid / 2),
+		(end->y * fov_scale_y) / end->z + (data->win_len / 2), 0, (t_col){0, 0, 0}};
 	// *start = (t_point){start->x + (data->win_wid / 2),
 	// 	start->y + (data->win_len / 2), 0, start->col};
 	// *end = (t_point){end->x + (data->win_wid / 2),
